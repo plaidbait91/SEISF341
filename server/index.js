f81fb0e494a0422f8575cfa8f364b00ef82356b7
Anish Kacham <f20190091@hyderabad.bits-pilani.ac.in>
2022-02-22 02:34:40 +0530

    feat: MongoDB Atlas connection ✨

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+// connect to mongoDB atlas using mongoose
+mongoose.connect( process.env.MONGODB_API_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+// checking if the connection to MongoDB atlas was successful
+const db = mongoose.connection;
+db.on("error",console.error.bind(console, "connection error: "));
+db.once("open", function () {
+	console.log("MongoDB Atlas Connection Successful");
+  });
+
+  // Basic endpoint of the backend
 app.get('/',(req,res)=>{
 	res.status(200).json({api:'version 1'});
 });
 
+// start the app on the specified port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>console.log('server started on port',PORT));

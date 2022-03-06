3ebe73147f1b6c0bdc98edd4ddd1e50670774944
plaidbait91 <75108809+plaidbait91@users.noreply.github.com>
2022-03-06 20:45:53 +0530

    Simplify user schema and JWT auth (#3)
    * Add JWT authentication for modifying database
    
    * Fix delete bug
    
    * refactor(layout): Changed react router
    
    * Simplify user schema
    
    Co-authored-by: Anish Kacham <f20190091@hyderabad.bits-pilani.ac.in>

--- server/models/user.js
+++ server/models/user.js
@@ -1,28 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+ 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-
-    password: {
-        type: String,
-        required: true
-    },
-
+ 
     fullName: {
         type: String,
         required: true
     },
-
+ 
     email: {
         type: String,
         required: true
     }
+    
 })
-
+ 
 const User = mongoose.model('User', userSchema);
 module.exports = {
     userSchema, 

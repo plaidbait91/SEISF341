0ccec31e3f9cc74368adb931becddc2622ddadd5
Ruban SriramBabu <s.ruban2000@gmail.com>
2022-04-10 12:27:59 +0530

    Implemented Tags and basic Profile (#13)

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,17 @@
 import { useState } from "react";
+import { Grid, GridItem, Image, Heading } from "@chakra-ui/react";
 
 export default function Profile(){
     return(
-        <h1>
-            DISPLAY PROFILE
-        </h1>
+        <Grid templateColumns='repeat(7, 1fr)'>
+            <GridItem margin="20px" colStart={1}>
+                <Image src={localStorage.getItem('pplink')}></Image>
+            </GridItem>
+            <GridItem align="left" colSpan={2}>
+                <Heading size="md">Name: {localStorage.getItem('name')}</Heading>
+                <Heading size="md">Email: {localStorage.getItem('email')}</Heading>
+                <Heading size="md">Total Upvotes: </Heading>
+            </GridItem>
+        </Grid>
     )
 }

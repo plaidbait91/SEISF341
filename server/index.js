bf3cfa852adb9de1fce6aed5eea4c9eae01748b7
Anish Kacham <f20190091@hyderabad.bits-pilani.ac.in>
2022-04-16 10:14:46 +0530

    fix(cors): added heroku backend

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const User = _.User
  
 const app = express();
 app.use(express.json())
-app.use(cors({origin: "http://localhost:3000"}));
+app.use(cors({origin: ["http://localhost:3000", "https://g-overflow.herokuapp.com/"]}));
 // connect to mongoDB atlas using mongoose
 mongoose.connect( process.env.MONGODB_API_URI,
   {

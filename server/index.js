cad62a3d696591f850c670e331fb777edab756c3
Anish Kacham <f20190091@hyderabad.bits-pilani.ac.in>
2022-04-16 10:18:18 +0530

    fix: nuub stuff

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const User = _.User
  
 const app = express();
 app.use(express.json())
-app.use(cors({origin: ["http://localhost:3000", "https://g-overflow.herokuapp.com/"]}));
+app.use(cors({origin: ["http://localhost:3000", "https://g-overflow.netlify.app/"]}));
 // connect to mongoDB atlas using mongoose
 mongoose.connect( process.env.MONGODB_API_URI,
   {
